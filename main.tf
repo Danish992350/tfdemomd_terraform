e1d085047888ac0edeaa59665cb662d74961a178
danish-cd <danish.shaikh@clouddrove.com>
2026-02-26 19:40:09 +0530

    Initial clean terraform commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Get latest Amazon Linux 2 AMI automatically
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_instance" "demo_123ec2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnets.default.ids[0]
+
+  tags = {
+    Name = "tf-demo-ec2"
+  }
+}
